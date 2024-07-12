aeb5d172e5f65fbd3b1a35afe2a93ce034854375
praniteem <pranitee23.majukar@gmail.com>
2024-07-11 20:17:55 -0400

    Talend Task with SQL queries

---
+++ Project_Talend/talendProduct.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE data_product; 
+
+USE data_product;
+SELECT * FROM dbo.stg_data;
+
+SELECT * FROM table_account;
+SELECT * FROM table_transfer;
+SELECT * FROM table_product;
+
+SELECT * FROM table_final;
